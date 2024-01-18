7852dad02d0c5f6f113212d2d06c6d9a98a86a88
miahro <mikko.ahro@gmail.com>
2024-01-18 12:09:33 +0200

    tcr: tests pass

--- tdd-mooc-small-steps/src/prices.mjs
+++ tdd-mooc-small-steps/src/prices.mjs
@@ -29,6 +29,8 @@ function createApp(database) {
     }
   }
 
+  function parseDate2(dateString){}
+
   function calculateCost(age, type, date, baseCost) {
     if (type === "night") {
       return calculateCostForNightTicket(age, baseCost);
