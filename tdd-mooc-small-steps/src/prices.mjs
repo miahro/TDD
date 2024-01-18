f987393beae5687d73cadad0d073b78e4680bff7
miahro <mikko.ahro@gmail.com>
2024-01-18 14:58:25 +0200

    tcr: tests pass

--- tdd-mooc-small-steps/src/prices.mjs
+++ tdd-mooc-small-steps/src/prices.mjs
@@ -92,6 +92,7 @@ function createApp(database) {
 
   function isHoliday(date) {
     const holidays = database.getHolidays();
+    const date2 = convert(date)
     for (let row of holidays) {
       let holiday = new Date(row.holiday);
       if (
