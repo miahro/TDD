8e559240942dfb26e6c30dfe708a5613a887792a
miahro <mikko.ahro@gmail.com>
2024-01-18 15:55:53 +0200

    tcr: tests pass

--- tdd-mooc-small-steps/src/prices.mjs
+++ tdd-mooc-small-steps/src/prices.mjs
@@ -31,8 +31,7 @@ function createApp(database) {
 
   function parseDate2(dateString){
     if (dateString) {
-      return Temporal.PlainDate.from(dateString);
-    }
+      return Temporal.PlainDate.from(dateString);}
   }
 
   function calculateCost(age, type, date, baseCost) {
