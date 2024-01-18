55d40991ca83652682f05694bfaebda084ed6ddb
miahro <mikko.ahro@gmail.com>
2024-01-18 15:09:39 +0200

    tcr: tests pass

--- tdd-mooc-small-steps/src/prices.mjs
+++ tdd-mooc-small-steps/src/prices.mjs
@@ -99,7 +99,7 @@ function createApp(database) {
       if (
         date2 &&
         date2 .equals(holiday2) &&
-        convert(date).year === holiday.getFullYear() &&
+        //convert(date).year === holiday.getFullYear() &&
         convert(date).month - 1 === holiday.getMonth() &&
         convert(date).day === holiday.getDate()
       ) {
